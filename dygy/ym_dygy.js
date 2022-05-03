d6e174b2638041cdbe46b10bb8b5cecec56b266c
ssh1 <1319174340@qq.com>
2022-05-04 01:45:02 +0800

    抖音果园

--- dygy/ym_dygy.js
+++ dygy/ym_dygy.js
@@ -20,10 +20,9 @@
  * 
  * 抓 minigame.zijieapi.com  的包  浇一次水即可获取ck  
  * 
- * 还是不会的请百度或者群里求助：QQ群：1001401060  tg：https://t.me/yml_tg  通知：https://t.me/yml2213_tg
+ * 还是不会的请百度或者群里求助：QQ群：884234287  tg：https://t.me/yml_tg  通知：https://t.me/yml2213_tg
  */
-const jsname = "抖音果园";
-const $ = Env(jsname);
+const $ = new Env("抖音果园");
 const notify = $.isNode() ? require('./sendNotify') : '';
 const Notify = 1; //0为关闭通知，1为打开通知,默认为1
 const debug = 0; //0为关闭调试，1为打开调试,默认为0
@@ -117,8 +116,9 @@ let choose_gold_num = 1;
 			await watering(ck);
 			await $.wait(2 * 1000);
 
-			await SendMsg(msg);
 		}
+		await SendMsg(msg);
+
 	}
 
 })()
